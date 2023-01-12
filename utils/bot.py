5cc9c3dbdec2acb97bd488ce25e9dda0d8a804a2
Starcea <stardev.uwu@gmail.com>
2023-01-12 11:18:50 +0900

    fix: utils path

--- utils/bot.py
+++ utils/bot.py
@@ -9,8 +9,8 @@ from discord.ext import commands  # noqa
 
 from config import BAD, STATUS
 from constants import OPTION_TYPES
-from crypt import AESCipher
-from logger import setup_logging
+from utils.crypt import AESCipher
+from utils.logger import setup_logging
 
 
 class Bot(commands.Bot):  # noqa

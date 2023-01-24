3be3bee228dad677a445116d26ef8797a0a3886a
Starcea <stardev.uwu@gmail.com>
2023-01-24 16:36:27 +0900

    feat: dev command in dev guild

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 from typing import Final
 
 TEST_GUILD_ID: Final[list[int]] = []
+DEV_GUILD_ID: Final[list[int]] = []
 STATUS: Final[str] = ""
 COLOR: Final[int] = 0x2f3136
 BAD: Final[int] = 0xed4245
--- functions/dev.py
+++ functions/dev.py
@@ -6,50 +6,44 @@ from discord import ApplicationContext, Embed, Option
 from discord.ext import commands
 
 from classes import Bot
-from config import BAD, COLOR
+from config import BAD, COLOR, DEV_GUILD_ID
 from utils import slash_command
 
 logger = getLogger(__name__)
 
 
-class Dev(commands.Cog, command_attrs={"hidden": True}):
+class Dev(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @slash_command()
-    @commands.is_owner()
+    @slash_command(guild_ids=DEV_GUILD_ID)
     async def stop(self, ctx: ApplicationContext):
         await ctx.respond("stopping..", ephemeral=True)
         await self.bot.close()
 
-    @slash_command()
-    @commands.is_owner()
+    @slash_command(guild_ids=DEV_GUILD_ID)
     async def load_ext(self, ctx: ApplicationContext, ext_name: Option(str)):
         self.bot.load_extension(f"functions.{ext_name}")
         await ctx.respond(f"{ext_name}.py loaded", ephemeral=True)
 
-    @slash_command()
-    @commands.is_owner()
+    @slash_command(guild_ids=DEV_GUILD_ID)
     async def unload_ext(self, ctx: ApplicationContext, ext_name: Option(str)):
         self.bot.unload_extension(f"functions.{ext_name}")
         await ctx.respond(f"{ext_name}.py loaded", ephemeral=True)
 
-    @slash_command()
-    @commands.is_owner()
+    @slash_command(guild_ids=DEV_GUILD_ID)
     async def reload_ext(self, ctx: ApplicationContext, ext_name: Option(str)):
         self.bot.unload_extension(f"functions.{ext_name}")
         self.bot.load_extension(f"functions.{ext_name}")
         await ctx.respond(f"{ext_name}.py reloaded", ephemeral=True)
 
-    @slash_command()
-    @commands.is_owner()
+    @slash_command(guild_ids=DEV_GUILD_ID)
     async def send_here(self, ctx: ApplicationContext, content: Option(str)):
         await ctx.send(content)
         delete_this = await ctx.respond("random respond")
         await delete_this.delete_original_response()
 
-    @slash_command()
-    @commands.is_owner()
+    @slash_command(guild_ids=DEV_GUILD_ID)
     async def sql(self, ctx: ApplicationContext, sql: Option(str)):
         try:
             result = await self.bot.db.execute(sql)
@@ -62,16 +56,14 @@ class Dev(commands.Cog, command_attrs={"hidden": True}):
             embed.add_field(name="Error", value=f"```py\n{format_exc().splitlines()[-1]}```")
         await ctx.respond(embed=embed, ephemeral=True)
 
-    @slash_command()
-    @commands.is_owner()
+    @slash_command(guild_ids=DEV_GUILD_ID)
     async def encrypt(self, ctx: ApplicationContext, content: Option(str)):
         embed = Embed(title="Encrypted!", color=COLOR)
         embed.add_field(name="Content", value=f"```{content}```")
         embed.add_field(name="Result", value=f"```{await self.bot.crypt.encrypt(content)}```")
         await ctx.respond(embed=embed, ephemeral=True)
 
-    @slash_command()
-    @commands.is_owner()
+    @slash_command(guild_ids=DEV_GUILD_ID)
     async def decrypt(self, ctx: ApplicationContext, content: Option(str)):
         embed = Embed(title="Decrypted!", color=COLOR)
         embed.add_field(name="Content", value=f"```{content}```")

b24fef0d06b88edd6b935a6a0d3bfeec5c6de65f
Starcea <stardev.uwu@gmail.com>
2023-01-29 22:40:24 +0900

    perf: unused Finals

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 from typing import Final
 
-TEST_GUILD_ID: Final[list[int]] = []  # 테스트 서버 ID
-DEV_GUILD_ID: Final[list[int]] = []  # 개발자 커맨드 서버 ID
 STATUS: Final[str] = ""  # 상태 메시지
 COLOR: Final[int] = 0x2f3136  # 일반 임베드 색상
 BAD: Final[int] = 0xed4245  # 오류/위험 임베드 색상
